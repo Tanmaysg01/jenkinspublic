7a96f9311398a2ff15e7588f51127f9aa836510b
Tanmaysg01 <tanmaygarge@gmail.com>
2024-01-08 14:03:01 +0530

    provider

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA4XZF6GOQKAHD4QMF"
+  secret_key = "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"
+}
---
+++ s3.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "jenkins-terraform-bucket-aws"
+
+}
